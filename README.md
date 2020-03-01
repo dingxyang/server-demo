35b0f39957d7a933e59528920e570f056e4a3757
esun <xydinga@163.com>
2020-03-01 16:07:58 +0800

    webpack自动打包盒服务器自动重启

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
+# build
+
+npm run build
+
 # start
-node app.js
+
+npm run start
+
+### 自动打包自动重启
+
+// --watch 自动打包
+npm install nodemon -g
--- src/index.js
+++ src/index.js
@@ -2,18 +2,24 @@
  * @Author: Yang
  * @Date: 2020-03-01 13:51:15
  * @LastEditors: Yang
- * @LastEditTime: 2020-03-01 15:48:17
+ * @LastEditTime: 2020-03-01 16:07:17
  * @Descripttion:
  * @FilePath: /server-demo/src/index.js
  */
 import express from "express";
-const app = express();
-const port = 3000;
 import Home from "./containers/Home";
 import React from "react";
 import { renderToString } from "react-dom/server";
 
+// 客户端渲染
+// React代码在浏览器器执行，消耗的是用户浏览器的性能
+
+// 服务器渲染
+// React代码在服务器上执行，消耗服务器端的性能
+
+const app = express();
 const content = renderToString(<Home />);
+
 app.get("/", (req, res) =>
   res.send(
     `
@@ -29,4 +35,4 @@ app.get("/", (req, res) =>
   )
 );
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(3000);
