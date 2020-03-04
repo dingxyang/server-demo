84a20a835b04943f4e6987fd7f576cbbae71de23
esun <xydinga@163.com>
2020-03-04 20:13:09 +0800

    context

--- src/pages/Exception/404.js
+++ src/pages/Exception/404.js
@@ -2,11 +2,24 @@
  * @Author: Yang
  * @Date: 2020-03-03 21:54:27
  * @LastEditors: Yang
- * @LastEditTime: 2020-03-03 21:57:05
+ * @LastEditTime: 2020-03-04 20:11:47
  * @Descripttion:
  * @FilePath: /server-demo/src/pages/Exception/404.js
  */
-import React from "react";
-export default () => {
-  return <div>404</div>;
-};
+import React, {Component} from "react";
+
+class NotFound extends Component {
+
+  componentWillMount() {
+    const {staticContext} = this.props;
+    if(staticContext) {
+      staticContext.NOT_FOUND = true
+    }
+  }
+
+  render() {
+    return <div>404</div>;
+  }
+
+}
+export default NotFound;
--- src/server/index.js
+++ src/server/index.js
@@ -2,17 +2,20 @@ import express from "express";
 import { render } from "./utils";
 import routes from "../Routes";
 import { matchRoutes } from "react-router-config";
-import {getStore} from "../store";
-import proxy from 'express-http-proxy';
+import { getStore } from "../store";
+import proxy from "express-http-proxy";
 
 const app = express();
 app.use(express.static("public"));
 
-app.use('/api',proxy('http://47.95.113.63', {
-  proxyReqPathResolver: function (req) {
-    return '/ssr/api'+req.url;
-  }
-}));
+app.use(
+  "/api",
+  proxy("http://47.95.113.63", {
+    proxyReqPathResolver: function(req) {
+      return "/ssr/api" + req.url;
+    }
+  })
+);
 
 app.get("*", (req, res) => {
   const store = getStore(req);
@@ -28,8 +31,15 @@ app.get("*", (req, res) => {
     }
   });
   Promise.all(promises).then(() => {
-    res.send(render(store, routes, req))
-  })
+    const context = {};
+    const html = render(store, routes, req, context);
+    if (context.NOT_FOUND) {
+      res.status(404)
+      res.send(html);
+    } else {
+      res.send(html);
+    }
+  });
 });
 
 app.listen(3000);
