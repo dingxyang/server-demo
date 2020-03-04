02ae460d9e7dafd9de4f8cb164e35c4c9945bbcf
esun <xydinga@163.com>
2020-03-04 21:33:24 +0800

    color

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -10,7 +10,7 @@ class Home extends React.Component {
 
   render() {
     return (
-      <div>
+      <div className={style.test}>
         {this.queryList()}
         <div>
           <button onClick={() => {alert("click")}}>
